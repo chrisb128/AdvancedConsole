9b9e1e2e2329405c4618da87489a72b6c1730e86
Chris Brown <chrisb128@gmail.com>
2020-05-10 13:00:10 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-scp -C -i ${DEPLOY_ID_KEY_LOCATION} ./out/storage.zip chris@status.chrisb.info:~/release/storage.zip
-scp -C -i ${DEPLOY_ID_KEY_LOCATION} ./out/api.zip chris@status.chrisb.info:~/release/api.zip
-scp -C -i ${DEPLOY_ID_KEY_LOCATION} ./out/client.zip chris@status.chrisb.info:~/release/client.zip
-ssh -i ${DEPLOY_ID_KEY_LOCATION} chris@status.chrisb.info \
-  docker load release/storage.zip \
-  && docker load release/api.zip \
-  && docker load release/client.zip \
-  && docker-compose -f docker/docker-compose.yml down \
-  && docker-compose -f docker/docker-compose.yml up
+#!/bin/sh
+
+scp -Crq -i $DEPLOY_ID_KEY_LOCATION ./out/ chris@status.chrisb.info:/tmp/advanced-console/
+ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker load -i /tmp/advanced-console/out/storage.zip
+ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker load -i /tmp/advanced-console/out/api.zip
+ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker load -i /tmp/advanced-console/out/client.zip
+ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker-compose -f docker/docker-compose.yml down
+ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker-compose -f docker/docker-compose.yml up -d

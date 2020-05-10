09c3d14babb22f007dc64906742540e4c1f4a3fa
Chris Brown <chrisb128@gmail.com>
2020-05-10 17:46:51 -0500

    Add some info logging to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-scp -Crq -i $DEPLOY_ID_KEY_LOCATION ./out/ chris@status.chrisb.info:/tmp/advanced-console/
+echo Authorization key located at $DEPLOY_ID_KEY_LOCATION
+
+echo Sending files to server
+scp -Cr -i $DEPLOY_ID_KEY_LOCATION ./out/ chris@status.chrisb.info:/tmp/advanced-console/
+
+echo Loading images
 ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker load -i /tmp/advanced-console/out/storage.zip
 ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker load -i /tmp/advanced-console/out/api.zip
 ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker load -i /tmp/advanced-console/out/client.zip
+
+echo Taking down
 ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker-compose -f docker/docker-compose.yml down
+
+echo Bringing back up
 ssh -i $DEPLOY_ID_KEY_LOCATION chris@status.chrisb.info docker-compose -f docker/docker-compose.yml up -d
